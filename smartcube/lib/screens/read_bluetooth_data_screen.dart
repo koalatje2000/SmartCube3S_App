092582adde811d12a42195e39f9d2473bb691c1a
joris-wes <99329100+joris-wes@users.noreply.github.com>
2024-11-11 17:29:20 +0100

    Removed unused class

--- smartcube/lib/screens/read_bluetooth_data_screen.dart
+++ smartcube/lib/screens/read_bluetooth_data_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:flutter/services.dart'; // Import Clipboard class
-import 'dart:convert';
+import 'package:flutter/services.dart';
 
 class ReadDataScreen extends StatefulWidget {
   final BluetoothDevice device;
